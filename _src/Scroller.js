cfe988f34798db1cd2533f6ac984ccb319a917cf
Matt Finucane <matfin@gmail.com>
2014-10-11 19:24:00 +0200

    Updating bower dependencies and kicking off work on the Gruntfile.

---
+++ _src/Scroller.js
@@ -0,0 +1,28 @@
+/**
+ *	The Scroller class populates the scrollable viewport with content
+ *	
+ *	@class Scroller
+ */
+
+function Scroller() {
+
+	/**
+	 *	The number of items that need to be 
+	 *	fetched to fill the viewport
+	 *
+	 *	@property 	numItemsRequired
+	 *	@type		{Number}
+	 */
+	this.numItemsRequired;
+};
+
+/**
+ *	Function to set up the scroller
+ *	
+ *	@function init
+ *	@param	{String} scrollerContainer - Selector for the scroller container
+ */
+Scroller.prototype.init = function(scrollerContainer) {
+
+};
+
