236f2962c6099a81e549007ce8c927e656855cf2
Matt Finucane <matfin@gmail.com>
2014-10-12 16:59:37 +0200

    Setting up event listeners for Scroller class with event names and callbacks.

--- _src/Scroller.js
+++ _src/Scroller.js
@@ -22,15 +22,62 @@ function Scroller() {
 	 *	@type		{Number}
 	 */
 	this.numItemsPresent;
+
+	/**
+	 *	DOM elment for the scroll container
+	 *	
+	 *	@property	scrollerContainer
+	 *	@type 		{Object}
+	 */
+	this.scrollerContainer;
 };
 
 /**
  *	Function to set up the scroller
  *	
  *	@function init
- *	@param	{String} scrollerContainer - Selector for the scroller container
+ *	@param	{Object} scrollerContainer - HTML DOM element for the scroller container
  */
 Scroller.prototype.init = function(scrollerContainer) {
-	console.log('Hello from the scroller');
+	this.scrollerContainer = scrollerContainer;
 };
 
+/**
+ *	Function to set up events for the scroller
+ *	
+ *	@function setupEvents
+ *	@param {Object} events - a list of events and their associated callbacks
+ *					For example, the following objects are valid
+ *
+ *					events = [
+ *						{
+ *							name: 'mouseover',
+ *							callback: function() {
+ *								console.log('Mouseover detected')	
+ *							}
+ *						},
+ *						{
+ *							name: 'click',
+ *							callback: function() {
+ *								console.log('Click event registered');
+ *							}
+ * 						}	
+ *					]
+ */
+Scroller.prototype.setupEvents = function(events) {
+	
+	/**
+	 *	Checking parameters
+	 */
+	if(typeof events === 'undefined' || typeof events !== 'object') {
+		console.log('Events param must be an array. You sent through: ', typeof events);
+		return;
+	}	
+
+	/**
+	 *	Then loop through the events and add the event listeners with callbacks
+	 */
+	for(var i in events) {
+		this.scrollerContainer.addEventListener(events[i].name, events[i].callback);
+	}
+};
